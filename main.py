5c25c1b7b5fe9754d51e702a9692aebaa2f78471
safwannaveed2 <safwansunka@gmail.com>
2026-02-15 14:54:56 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import tensorflow as tf
+import pickle
+
+# Load model
+model = tf.keras.models.load_model("next_word_model.h5")
+
+# Load tokenizer
+with open("tokenizer.pkl", "rb") as f:
+    tokenizer = pickle.load(f)
+
+# For padding sequences
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+max_sequence_len = 5  # Same as used in training
+
+app = FastAPI()
+
+class TextInput(BaseModel):
+    text: str
+
+@app.post("/predict")
+def predict_next_word(input: TextInput):
+    text = input.text
+    token_list = tokenizer.texts_to_sequences([text])[0]
+    token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+    predicted_index = model.predict(token_list, verbose=0).argmax(axis=1)[0]
+
+    for word, index in tokenizer.word_index.items():
+        if index == predicted_index:
+            return {"next_word": word}
+    
+    return {"next_word": "N/A"}
